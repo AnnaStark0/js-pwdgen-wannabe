a9b0a640269bc7c67bc810c32217f75e45530b58
Annastark <89winteriscoming@gmail.com>
2021-11-04 15:54:07 +0100

    getElementById

--- js/myscript.js
+++ js/myscript.js
@@ -4,3 +4,6 @@ const cognome = prompt ('Inserisci il tuo Cognome');
 
 const colore = prompt ('Inserisci il tuo Colore Preferito');
 
+const password = nome + cognome + colore + '21';
+
+document.getElementById('psw').innerHTML = 'Ecco la tua Password: ' + password;
