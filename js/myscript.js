fbfc4d9478d8de77027469d3cc997419eb9ab9ec
Annastark <89winteriscoming@gmail.com>
2021-11-04 16:07:49 +0100

    title

--- js/myscript.js
+++ js/myscript.js
@@ -9,3 +9,9 @@ const colore = prompt ('Inserisci il tuo Colore Preferito');
 const password = nome + cognome + colore + '21';
 
 document.getElementById('psw').innerHTML = 'Ecco la tua Password: ' + password;
+
+
+
+
+
+console.log (password);
